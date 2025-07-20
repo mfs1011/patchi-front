791811ec297b31e13d647ada0624a9e6fd13722e
murodovicch <istamovmirshod74@gmail.com>
2025-07-20 18:50:36 +0500

    mercure

--- src/views/admin/clientsB2C/ClientsB2C.vue
+++ src/views/admin/clientsB2C/ClientsB2C.vue
@@ -138,7 +138,7 @@ function connectMercure() {
         const eventDataId = JSON.parse(event.data).eventId
 
         if (eventDataId === 5) {
-            await customerStore.fetchCustomers(route.query);
+            await customerStore.fetchCustomers(route.query, false);
         }
     })
 }
